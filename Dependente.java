6f3f07d52fa1eb9041208cc3b3a05d24cb4e16a4
pooCaboFrio <poocabofrio@yahoo.com.br>
2013-10-18 21:22:38 -0300

    Create Dependente.java

---
+++ Dependente.java
@@ -0,0 +1,15 @@
+package modelo;
+
+public class Dependente extends Pessoa {
+    private Cliente Cl;
+
+
+    public Cliente getCl() {
+        return Cl;
+    }
+
+    public void setCl(Cliente Cl) {
+        this.Cl = Cl;
+    }
+        
+}
