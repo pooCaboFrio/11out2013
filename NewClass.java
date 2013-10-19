1b5d37a6eed3c2c2c072bb901c2e3e128e6fa1ca
pooCaboFrio <poocabofrio@yahoo.com.br>
2013-10-18 21:18:15 -0300

    Create NewClass.java

---
+++ NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author aluno
+ */
+public class NewClass {
+    
+    public static void main(String args[]){
+        NewJFrame test = new NewJFrame();
+    }
+}
