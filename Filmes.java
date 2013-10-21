81df9db82ce90128c4af9fc7a8e6d2c95cab0c7b
pooCaboFrio <poocabofrio@yahoo.com.br>
2013-10-20 23:53:32 -0200

    Update Filmes.java

--- Filmes.java
+++ Filmes.java
@@ -1,11 +1,30 @@
-
+/**
+ *
+ * @author moraes
+ */
 public class Filmes {
-	private String nome;
-	private int categoria; //0 = ação, 1 = comédia, 2 = terror, 3 = romance
-	
+	/*
+	 * TODO usar OU genero OU categoria
+	 * pode ter mais de um genero, basta separar por vírgula
+	 * só pode ter uma categoria
+	*/
+	private String nome,genero;
+	private int categoria; //0 = ação, 1 = comédia, 2 = terror, 3 = romance		
+
+	//TODO escolher apenas um construtor posteriormente
+	Filmes(String nome,String genero){
+		this.nome = nome;
+		this.genero = genero;
+	}
 	Filmes(String nome, int categoria){
 		this.nome = nome;
 		this.categoria = categoria;
+	}
+	public String getGenero(){
+		return genero;
+	}
+	public void setGenero(String genero){
+		this.genero = genero;
 	}
 	public String getNome() {
 		return nome;
@@ -19,6 +38,5 @@ public class Filmes {
 	public void setCategoria(int categoria) {
 		this.categoria = categoria;
 	}
-	
 	
 }
